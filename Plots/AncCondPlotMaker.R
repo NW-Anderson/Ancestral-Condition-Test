f482e497942c80162b23a257b2f750bf93a8ac42
Nathan <nathan.wally.anderson@gmail.com>
2019-06-27 19:16:38 -0500

    fixed plots a bit

--- Plots/AncCondPlotMaker.R
+++ Plots/AncCondPlotMaker.R
@@ -178,12 +178,7 @@ fig_label('D:',cex = 2.5)
 ##### Fig 2 #####
 par(mfrow = c(1,1), mar = c(5,4,4,2) + .1)
 load('AncCondFig2DataPostBlackmon.RData')
-# data <- cbind(rep(1:10, each = 100), 
-#               as.vector(fig2.data))
-# colnames(data) <- c('Scale.Factor','Pval')
-# 
-# data <- as.data.frame(data)
-# ggraptR(data)
+
 
 
 x <- rep(1:10, each=100)
@@ -234,7 +229,7 @@ abline(h = .05, lty = 2, lwd = .7)
 
 
 ##### Fig 4 #####
-load('AncCondFig4DataPostBlackmon.RData')
+load('AncCondFig4DataPostBlackmonp3.RData')
 
 x <- rep(1:10, each=200)
 x <- jitter(x, factor=1.5)
@@ -252,25 +247,25 @@ for(i in 1:10){
   probs[i] <- paste(
     as.character(
       round(100 * sum(y[(200 * (i - 1) + 1):(200 * i)] <= .025, na.rm = T) / sum(!is.na(y[(200 * (i - 1) + 1):(200 * i)])),
-            digits = 1)),'%')
+            digits = 0)),'%')
   probs2[i] <- paste(
     as.character(
       round(100 * sum(y[(200 * (i - 1) + 1):(200 * i)] <= .05, na.rm = T) / sum(!is.na(y[(200 * (i - 1) + 1):(200 * i)])),
-            digits = 1)),'%')
+            digits = 0)),'%')
 }
 plot(x = x, y = y, xaxt="n",xlab="", ylab= "", pch=16,cex=.6)
-mtext(probs, side=3, at=1:10, cex=.7, col = 'red')
-mtext(probs2, side = 3, at = 1:10, cex = .7, line = .6)
+mtext(probs, side=3, at=1:10, cex=.7)
+# mtext(probs2, side = 3, at = 1:10, cex = .7, line = .6, col = 'red')
 mtext(1:10, side=1, at=1:10, cex=.85)
 mtext("Scaling Factor", side=1, line=1)
 mtext("p-value", side=2, line=2.2)
-abline(h = .025, lty = 2, lwd = .7, col = 'red')
-abline(h = .05, lty = 2, lwd = .7)
+abline(h = .025, lty = 2, lwd = .7)
+# abline(h = .05, lty = 2, lwd = .7)
 
 
 ##### Fig 5 #####
-load('AncCondFig5DataPostBlackmon.RData')
-#### with rate = 3 there is still 14%NA values. Should I go higher??? ####
+load('AncCondFig5DataPostBlackmonp3.RData')
+# with rate = 3 there is still 14%NA values. Should I go higher??? #
 x <- rep(seq(from=20, to=200, by=20), each=200)
 x <- jitter(x, factor=1.5)
 y <- vector()
@@ -289,21 +284,21 @@ for(i in 1:10){
   probs[i] <- paste(
     as.character(
       round(100 * sum(y[(200 * (i - 1) + 1):(200 * i)] <= .025, na.rm = T) / sum(!is.na(y[(200 * (i - 1) + 1):(200 * i)])),
-            digits = 1)),'%')
+            digits = 0)),'%')
   probs2[i] <- paste(
     as.character(
       round(100 * sum(y[(200 * (i - 1) + 1):(200 * i)] <= .05, na.rm = T) / sum(!is.na(y[(200 * (i - 1) + 1):(200 * i)])),
-            digits = 1)),'%')
+            digits = 0)),'%')
 }
 plot(x = x, y = y, xlab="", ylab= "", xaxt="n", 
      pch=16,cex=.6, xlim=c(10, 210))
 mtext(probs, 
-      side=3, at=seq(from=20, to=200, by=20), cex=.7, col = 'red')
-mtext(probs2, 
-      side = 3, at=seq(from=20, to=200, by=20), cex = .7, line = .6)
+      side=3, at=seq(from=20, to=200, by=20), cex=.7)
+# mtext(probs2, 
+#       side = 3, at=seq(from=20, to=200, by=20), cex = .7, line = .6)
 mtext(c(20,40,60,80,100,120,140,160,180,200), side=1, 
       at=c(20,40,60,80,100,120,140,160,180,200), cex=.85)
 mtext("Taxa", side=1, line=1)
 mtext("p-value", side=2, line=2.2)
-abline(h = .025, lty = 2, lwd = .7, col = 'red')
-abline(h = .05, lty = 2, lwd = .7)
+abline(h = .025, lty = 2, lwd = .7)
+# abline(h = .05, lty = 2, lwd = .7)
